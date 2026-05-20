54557efd84c113a83647b37a81fc022f0e4f3403
dhyangabriel517-sys <dhyangabriel517@gmail.com>
2026-05-20 08:37:18 -0300

    Delete Atividade4.sql

--- Atividade4.sql
+++
@@ -1,9 +0,0 @@
-
-
-Pega os `id_do_autor` onde `id_do_autor = id_do_visualizador`, tira duplicados
-  com `DISTINCT` e ordena crescente.
-
-Query: `SELECT DISTINCT id_do_autor AS id FROM 
-  visualizacoes WHERE id_do_autor = id_do_visualizador ORDER BY id;`
-
-Resultado do exemplo: 4, 7
